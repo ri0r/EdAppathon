fde620cedd50123094bcadcd31fb3169e3311259
Roberto Bezoari <Roberto@dhcp-111-176.ExternalUser.inf.ed.ac.uk>
2011-10-08 13:54:21 +0100

    core-hello

--- src/we/kill/the/batman/teamnine/EdAppathonActivity.java
+++ src/we/kill/the/batman/teamnine/EdAppathonActivity.java
@@ -2,12 +2,15 @@ package we.kill.the.batman.teamnine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class EdAppathonActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello Austin");
         setContentView(R.layout.main);
     }
 }
