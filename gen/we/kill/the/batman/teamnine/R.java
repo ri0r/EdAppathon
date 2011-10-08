e3f959ce6d587d78032616e3c0d8dc848f19c794
Oleg Filippov <olegfilippov@c108003.wlan.net.ed.ac.uk>
2011-10-08 20:23:08 +0100

    oleg

--- gen/we/kill/the/batman/teamnine/R.java
+++ gen/we/kill/the/batman/teamnine/R.java
@@ -13,11 +13,23 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int again_button=0x7f050000;
+        public static final int endLocationEditor=0x7f050002;
+        public static final int getRoadsButton=0x7f050003;
+        public static final int removeRoadsButton=0x7f050004;
+        public static final int startLocationEditor=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int listroutesview=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int endLocation=0x7f040003;
+        public static final int getPath=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int removeRoads=0x7f040005;
+        public static final int startLocation=0x7f040002;
     }
 }
