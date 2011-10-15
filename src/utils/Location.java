31621ec47a574d9d77ea325cc52ef006129f75e1
Maciej Pajak <pajak.m@gmail.com>
2011-10-15 17:58:50 +0100

    Commiting just the utils so they work.

--- src/utils/Location.java
+++ src/utils/Location.java
@@ -2,10 +2,18 @@ package utils;
 
 public class Location {
 	private String location;
+	private float longitude;
+	private float latitude;
 	
 	public Location(String name) {
 		this.location = name;
 	}
+	public Location(String name, float lat, float lng) {
+		this.longitude = lng;
+		this.latitude = lat;
+		this.location = name;
+	}
+	
 	public String getLocation() {
 		return location;
 	}
@@ -13,6 +21,26 @@ public class Location {
 	public void setLocation(String location) {
 		this.location = location;
 	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
--- src/utils/Utility.java
+++ src/utils/Utility.java
@@ -16,8 +16,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 
 public class Utility {
+	
+	private static final String TAG="Utility";
 	
 	public static List<Location> getRoads(String start, String end) {
 		
@@ -25,44 +29,72 @@ public class Utility {
 		StringBuilder sb = new StringBuilder();
 		sb.append("http://maps.googleapis.com/maps/api/directions/json?origin=");
 		
-		sb.append(start);
+		sb.append(start.trim());
 		
 		sb.append("&destination=");
-		sb.append(end);
+		sb.append(end.trim());
 		sb.append("&sensor=false");
-		String s = null;
+		sb.append("&alternatives=true&region=uk");
+		String s = sb.toString();
 		Pattern p = Pattern.compile("[\\s]");
+		Pattern road = Pattern.compile("[ABJM][0-9]+");
+		
 		JSONObject json = new JSONObject();
 		ArrayList<String> htmlInstructions = new ArrayList<String>();	
-
+		List<Location> edLocs = new ArrayList<Location>();
+		
 		try { 
+			Log.d(TAG,"1");
 			HttpClient hc = new DefaultHttpClient();
+			Log.d(TAG,"2");
 			HttpPost post = new HttpPost(s);
+			Log.d(TAG,"3");
 
 			HttpResponse rp = hc.execute(post);
 
 			if (rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 				json = new JSONObject(EntityUtils.toString(rp.getEntity()));
 			}
-			JSONArray jsonArr = json.getJSONArray("steps");
-			for (int i = 0; i < jsonArr.length(); i++) {
-				JSONObject jo = jsonArr.getJSONObject(i);
-				htmlInstructions.add(jo.getString("html_instructions"));
+			//ArrayList<String> routes = new ArrayList<String>();
+			JSONArray jroutes = json.getJSONArray("routes");
+			Log.d(TAG, "routes " + Integer.toString(jroutes.length()));
+			for (int j = 0; j < jroutes.length(); j++) {
+				JSONObject jroute = jroutes.getJSONObject(j);
+				JSONArray jlegs = jroute.getJSONArray("legs");
+				Log.d(TAG, "legs " + Integer.toString(jlegs.length()));
+				JSONObject jleg = jlegs.getJSONObject(0);
+				JSONArray jsteps = jleg.getJSONArray("steps");
+				Log.d(TAG, "Steps " + Integer.toString(jsteps.length()));
+				for (int i = 0; i < jsteps.length(); i++) {
+					JSONObject jstep = jsteps.getJSONObject(i);
+					String instr = jstep.getString("html_instructions");
+					JSONObject jstart = jstep.getJSONObject("start_location");
+					float lat = Float.parseFloat(jstart.getString("lat"));
+					float lng = Float.parseFloat(jstart.getString("lng"));
+					Matcher m = road.matcher(instr);
+					while (m.find()) {
+						
+						edLocs.add(new Location(m.group(0), lat, lng));
+						Log.d(TAG, "road added " + m.group(0));
+					}
+					//Log.d(TAG,jstep.getString("html_instructions"));
+					htmlInstructions.add(jstep.getString("html_instructions"));
+				}
 			}
 			
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		} catch (JSONException je) {
-			je.printStackTrace();
+//		} catch (JSONException je) {
+//			je.printStackTrace();
 		}
-		List<Location> edLocs = new ArrayList<Location>();
+		
 //		Pattern p = Pattern.compile("[AJM][0-9]+");
-		for (String test : htmlInstructions) {
-			Matcher m = p.matcher(test);
-			if (m.find()) {
-				edLocs.add(new Location(m.group(0)));
-			}
-		}
+//		for (String test : htmlInstructions) {
+//			Matcher m = p.matcher(test);
+//			if (m.find()) {
+//				edLocs.add(new Location(m.group(0)));
+//			}
+//		}
 		return edLocs;
 	}
 
